d18a910b1a25f33eed4f1ac95841a5194373f63b
rodrigob.itcs <rodrigob@itcsoluciones.com>
2023-03-25 13:36:15 -0300

    clase 2

---
+++ clase2/app.py
@@ -0,0 +1,23 @@
+
+#Importo Flask
+from flask import Flask
+
+#Importar libreria os
+import os
+
+#Importar funcion que crea la app
+from main import create_app
+import os
+
+
+#LLamada a la funcion que retorna la app
+app = create_app()
+
+#Hay que hacer push del contexto de la app
+#Permite no tener que pasar la variable app de arhivo en arhivo
+#De esta manera no vamos a tener importaciones reursivas
+
+app.app_context().push()
+
+if __name__ == '__main__': 
+    app.run(debug=True,port=os.getenv('PORT'))
---
+++ clase2/main/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+
+#Vamos a crear un metodo que inicializara la app y todos los modulos
+def create_app():
+    #inicio Flask
+    app = Flask(__name__)
+    
+    #variables de entono
+    load_dotenv()
+    #
+    #Aca iniciaremos los demas modulos de la app
+    #
+    
+    #Por ultimo retornamos la aplicacion iniializada
+    return app
