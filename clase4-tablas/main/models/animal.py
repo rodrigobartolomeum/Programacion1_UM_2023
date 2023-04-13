03d4c9545a3fcc9f631451b1a6e519320961f182
rodrigob.itcs <rodrigob@itcsoluciones.com>
2023-04-13 10:45:35 -0300

    Clase4-Tablas

---
+++ clase4-tablas/main/models/animal.py
@@ -0,0 +1,32 @@
+from .. import db
+
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    raza = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return '<Animal: %r >' % (self.raza)
+    #Convertir objeto en JSON
+    def to_json(self):
+        animal_json = {
+            'id': self.id,
+            'raza': str(self.raza),
+
+        }
+        return animal_json
+
+    def to_json_short(self):
+        animal_json = {
+            'id': self.id,
+            'raza': str(self.raza),
+
+        }
+        return animal_json
+
+    @staticmethod
+    #Convertir JSON a objeto
+    def from_json(animal_json):
+        id = animal_json.get('id')
+        raza = animal_json.get('raza')
+        return Animal(id=id,
+                    raza=raza,
+                    )
