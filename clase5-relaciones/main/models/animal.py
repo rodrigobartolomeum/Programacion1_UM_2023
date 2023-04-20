dcd510d9c59c036e1816b4783b1e23e2829950f2
rodrigob.itcs <rodrigob@itcsoluciones.com>
2023-04-20 06:18:00 -0300

    Clase5-relaciones

---
+++ clase5-relaciones/main/models/animal.py
@@ -0,0 +1,35 @@
+from .. import db
+
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    raza = db.Column(db.String(100), nullable=False)
+    #nombre de la relacion (historias)
+    historias = db.relationship("HistoriaClinica", back_populates="animal",cascade="all, delete-orphan")
+    
+    def __repr__(self):
+        return '<Animal: %r >' % (self.raza)
+    #Convertir objeto en JSON
+    def to_json(self):
+        animal_json = {
+            'id': self.id,
+            'raza': str(self.raza),
+
+        }
+        return animal_json
+
+    def to_json_short(self):
+        animal_json = {
+            'id': self.id,
+            'raza': str(self.raza),
+
+        }
+        return animal_json
+
+    @staticmethod
+    #Convertir JSON a objeto
+    def from_json(animal_json):
+        id = animal_json.get('id')
+        raza = animal_json.get('raza')
+        return Animal(id=id,
+                    raza=raza,
+                    )
