e70f9e186d7715a67e14c001843c1c0ff87cf686
rodrigob.itcs <rodrigob@itcsoluciones.com>
2023-04-26 20:54:41 -0300

    Manejo de fecha

--- clase5-relaciones/main/models/animal.py
+++ clase5-relaciones/main/models/animal.py
@@ -1,9 +1,11 @@
 from .. import db
 import json
-
+from datetime import datetime
 class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
     raza = db.Column(db.String(100), nullable=False)
+    fechaNac = db.Column(db.DateTime, nullable=False)
     #nombre de la relacion (historias)
     historias = db.relationship("HistoriaClinica", back_populates="animal",cascade="all, delete-orphan")
     
@@ -13,7 +15,9 @@ class Animal(db.Model):
     def to_json(self):
         animal_json = {
             'id': self.id,
+            'name': str(self.name),
             'raza': str(self.raza),
+            'fechaNac': str(self.fechaNac.strftime("%d-%m-%Y"))
         }
         return animal_json
     
@@ -21,7 +25,9 @@ class Animal(db.Model):
         historias = [historia.to_json() for historia in self.historias]
         animal_json = {
             'id': self.id,
+            'name': str(self.name),
             'raza': str(self.raza),
+            'fechaNac': str(self.fechaNac.strftime("%d-%m-%Y")),
             'historias':historias
 
         }
@@ -39,7 +45,11 @@ class Animal(db.Model):
     #Convertir JSON a objeto
     def from_json(animal_json):
         id = animal_json.get('id')
+        name = animal_json.get('name')
         raza = animal_json.get('raza')
+        fechaNac = datetime.strptime(animal_json.get('fechaNac'), '%d-%m-%Y')
         return Animal(id=id,
+                    name=name,
                     raza=raza,
+                    fechaNac = fechaNac
                     )
